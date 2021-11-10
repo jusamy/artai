06714f282e530bd56232683864efd1e73b7f5c60
Julien V <47949456+jusamy@users.noreply.github.com>
2021-11-10 17:15:06 +0100

    Update art2_ai.py

--- art2_ai.py
+++ art2_ai.py
@@ -3,7 +3,7 @@ from keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormaliza
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model, load_model
-#from keras.optimizers import Adam
+from keras.optimizers import Adam
 import numpy as np
 from PIL import Image
 import os
@@ -96,7 +96,7 @@ def save_images(cnt, noise):
 
 
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
-optimizer = keras.optimizers.Adam(1.5e-4, 0.5)
+optimizer = Adam(1.5e-4, 0.5)
 discriminator = build_discriminator(image_shape)
 discriminator.compile(loss='binary_crossentropy',optimizer=optimizer, metrics=['accuracy'])
 generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
